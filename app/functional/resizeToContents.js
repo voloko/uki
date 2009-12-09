085e8da8be676db962371472638b658797258a40
Vladimir Kolesnikov <voloko@gmail.com>
2009-12-09 10:59:04 +0300

    cleanup, resize to contents testing

---
+++ app/functional/resizeToContents.js
@@ -0,0 +1,15 @@
+uki({
+    view: 'Box', rect: '1000 600', anchors: 'left right bottom top', autosize: 'width height', background: '#CCF',
+    childViews: [
+        {view: 'Box', rect: '700 300 200 200', anchors: 'right bottom', background: '#CCC', childViews: {
+            view: 'Label', rect: '200 200', anchors: 'top left rigth', multiline: 'true',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            inset: '3 3', name: 'target', resizeToContents: true
+        }},
+        {view: 'Button', rect: '10 10 100 24', anchors: 'left top', text: 'resize', name: 'doIt' }
+    ]
+}).attachTo( document.getElementById('test'), '1000 600' )
+
+uki('[name=doIt]').click(function () {
+    uki('[name=target]').html(uki('[name=target]').html() + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit ');
+})
