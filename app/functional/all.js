38045d2a2738c1a77e95398a832be61c11bb99f0
Vladimir Kolesnikov <voloko@gmail.com>
2010-01-14 16:34:28 +0300

    min size and fit into 1024x768

--- app/functional/all.js
+++ app/functional/all.js
@@ -8,10 +8,10 @@ function elements() {
         { view: 'Checkbox', rect: '250px 35px 24px 24px',  anchors: 'left top', checked: true },
         { view: 'Label',    rect: '275px 35px 100px 24px', anchors: 'left top', text: 'Checkbox 2', selectable: false },
         
-        { view: 'Radio',    rect: '750px 10px 24px 24px',  anchors: 'left top', name: 'probe', group: 'radio_1' },
-        { view: 'Label',    rect: '775px 10px 100px 24px', anchors: 'left top', html: 'Radio 1', selectable: false },
-        { view: 'Radio',    rect: '750px 35px 24px 24px',  anchors: 'left top', checked: true, group: 'radio_1' },
-        { view: 'Label',    rect: '775px 35px 100px 24px', anchors: 'left top', text: 'Radio 2', selectable: false },
+        { view: 'Radio',    rect: '720px 10px 24px 24px',  anchors: 'left top', name: 'probe', group: 'radio_1' },
+        { view: 'Label',    rect: '745px 10px 100px 24px', anchors: 'left top', html: 'Radio 1', selectable: false },
+        { view: 'Radio',    rect: '720px 35px 24px 24px',  anchors: 'left top', checked: true, group: 'radio_1' },
+        { view: 'Label',    rect: '745px 35px 100px 24px', anchors: 'left top', text: 'Radio 2', selectable: false },
         
         { view: 'TextField',rect: '400 10 100 24',         anchors: 'left top', value: "Small input", placeholder: 'text' },
         { view: 'Button',   rect: '510px 10px 200px 24px', anchors: 'left top', text: 'button without focus!', focusable: false },
@@ -21,7 +21,7 @@ function elements() {
 }
 
 uki(
-    { view: 'Box', rect: '0 0 1000px 300px', anchors: 'top left right width', childViews: [
+    { view: 'Box', rect: '0 0 1000px 300px', minSize: '980 0', anchors: 'top left right width', childViews: [
         { view: 'Box', background: 'theme(panel)', rect: '0 0 1000px 100px',     anchors: 'top left right width', childViews: elements() },
         { view: 'Box',  rect: '0 150px 1000px 100px', anchors: 'top left right width', childViews: elements() }
     ]}
