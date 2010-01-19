d2e94737bdf3310435088842eb4ee59a636cf939
Vladimir Kolesnikov <voloko@gmail.com>
2010-01-19 12:43:49 +0300

    year up

--- src/uki-theamless.js
+++ src/uki-theamless.js
@@ -1,8 +1,8 @@
 /*!
  * uki JavaScript Library
- * Licensed under the MIT license
+ * Licensed under the MIT license http://ukijs.org/LICENSE
  *
- * Copyright (c) 2009 Vladimir Kolesnikov
+ * Copyright (c) 2010 Vladimir Kolesnikov
  *
  * Parts of code derived from jQuery JavaScript Library
  * Copyright (c) 2009 John Resig
