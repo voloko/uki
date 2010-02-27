936443663547f15ad335ab445980fe852adecdfb
Володя Колесников <voloko@gmail.com>
2010-02-27 20:37:52 +0300

    Toggle Button

---
+++ src/uki-more/more/view/toggleButton.js
@@ -0,0 +1,24 @@
+uki.view.declare('uki.more.view.ToggleButton', uki.view.Button, function(Base) {
+    
+    this._setup = function() {
+        Base._setup.call(this);
+        this._focusable = false;
+    };
+    
+    this.checked = uki.newProp('_checked', function(state) {
+        this._checked = !!state;
+        this._updateBg();
+    });
+    
+    this._updateBg = function() {
+        var name = this._disabled ? 'disabled' : this._down || this._checked ? 'down' : this._over ? 'hover' : 'normal';
+        this._backgroundByName(name);
+    };
+    
+    this._mouseup = function(e) {
+        if (!this._down) return;
+        this._down = false;
+        this.checked(!this.checked())
+    };
+    
+});
