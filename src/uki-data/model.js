b086b32d97e8291f56d6a4564492b3e4f1036fef
Володя Колесников <voloko@gmail.com>
2010-04-04 00:50:36 +0400

    loaders, dynamic accessor creation

--- src/uki-data/model.js
+++ src/uki-data/model.js
@@ -1,28 +1,94 @@
 include('observable.js');
 
+/**
+ *
+ * @example
+ * myModel = uki.newClass(uki.data.Model, function() {
+ *     uki.data.model.addField(this, ['name', 'age', 'sex']);
+ * })
+ *
+ * var m = new myModel({ age: 22, name: 'Jonh Smith', sex: 'm' })
+ * m.bind('change.sex', function() {
+ *     alert('omg! wtf!');
+ * });
+ *
+ * m.bind('change', function(e) {
+ *     console.log(e.fields);
+ * });
+ *
+ * m.name('Joe Black') // triggers 'change' and 'change.name'
+ * m.sex('w') // triggers'change' and 'change.sex'
+ */
 uki.data.Model = uki.newClass(uki.data.Observable, function(Observable) {
     
-    this.change = this.update = function(values, arg2) {
-        var changes = {}, fields = [];
-        if (arg2 !== undefined) {
-            var tmp = {};
-            tmp[values] = arg2;
-            values = tmp;
-        }
-        uki.each(values, function(i) {
-            if (this[i] != values[i]) {
-                changes[i] = true
-                fields.push(i);
-                this[i] = values[i];
+    this._fields = [];
+    
+    this.init = function(values) {
+        this.update(values || {});
+    };
+    
+    this.fields = function() {
+        return this._fields;
+    };
+    
+    this.update = function(values) {
+        var fields = [], changes = {};
+        
+        uki.each(values, function(name, value) {
+            var field = uki.isFunction(this[name]) ? '_' + name : name;
+            if (this[field] != value) {
+                changes[name] = true;
+                fields.push(name);
+                this[field] = value;
+                this.trigger('change.' + name, { model: this });
             }
         }, this);
         
-        this._triggerChange({changes: changes, fields: fields, model: this});
+        if (fields.length) this.trigger('change', {changes: changes, fields: fields, model: this});
         return this;
     };
     
-    this._triggerChange = function(e) {
-        this.trigger('change', e);
-    };
-    
 });
+
+uki.data.model = {
+    _newProp: function(name) {
+        return uki.newProp('_' + name, function(v) {
+            var changes = {};
+            changes[name] = v;
+            this.update(changes);
+        });
+    },
+    
+    addFields: function(target, names) {
+        for (var i=0; i < names.length; i++) {
+            target[names[i]] = uki.data.model._newProp(names[i]);
+        }
+        // do not break prototypes
+        target._fields = names.concat(target._fields || []);
+    },
+    
+    newLoader: function(name, options) {
+        return function(callback) {
+            callback = callback || uki.F;
+            if (this['loaded.' + name]) {
+                callback.call(this, this[name]());
+            } else {
+                this.bind('load.' + name, callback);
+                if (!this['loading.' + name]) {
+                    this['loading.' + name] = true;
+                    uki.ajax(uki.extend({
+                        url: uki.isFunction(options.url) ? options.url.call(this) : options.url,
+                        data: uki.isFunction(options.data) ? options.data.call(this) : options.data ? options.data : { id: this.id() },
+                        success: uki.proxy(function(val) {
+                            this['loading.' + name] = false;
+                            this['loaded.' + name] = true;
+                            options.setter ? options.setter.call(this, val) : this['_' + name] = val;
+                            this.trigger('load.' + name, val);
+                            this.unbind('load.' + name);
+                        }, this)
+                    }, options.ajaxOptions || {}));
+                }
+            }
+        };
+    }
+};
