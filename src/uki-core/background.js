9e8b04b79204808961b80361564f6c58f9ee4d7c
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-01 20:14:56 +0700

    background.js: small opts

--- src/uki-core/background.js
+++ src/uki-core/background.js
@@ -19,12 +19,12 @@ include('uki.js');
      * @namespace
      * @returns {uki.background.Base} created background
      */
-    var self = uki.background = function(bg) {
+    var uki$theme = uki$theme, self = uki.background = function(bg) {
         if (typeof(bg) === 'string') {
             var match;
             /*jsl:ignore*/
             if ( match = bg.match(nullRegexp) ) return new self.Null();
-            if ( match = bg.match(themeRegexp) ) return uki.theme.background( match[1] );
+            if ( match = bg.match(themeRegexp) ) return uki$theme.background( match[1] );
             if ( match = bg.match(rowsRegexp) ) return new self.Rows( match[1].split(',')[0], match[1].split(/\s*,\s*/).slice(1) );
             if ( match = bg.match(cssBoxRegexp) ) return new self.CssBox( match[1] );
             /*jsl:end*/
