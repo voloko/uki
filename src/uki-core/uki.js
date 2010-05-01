acf9fe71e2c38037e6f185b5c6881ac42c1c114b
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-01 19:51:16 +0700

    uki.js: small optimizations of comparison of types

--- src/uki-core/uki.js
+++ src/uki-core/uki.js
@@ -11,16 +11,19 @@ include('const.js');
  * @return {uki.Collection}
  */
 root.uki = root.uki || function(val, context) {
-    if (typeof val == 'string') {
+    if (typeof val === "string") {
+	
         var m = val.match(/^#((?:[\w\u00c0-\uFFFF_-]|\\.)+)$/),
             e = m && uki._ids[m[1]];
         if (m && !context) {
             return new uki.Collection( e ? [e] : [] );
         }
         return uki.find(val, context);
+		
     }
     if (val.length === undefined) val = [val];
     if (val.length > 0 && uki.isFunction(val[0].typeName)) return new uki.Collection(val);
+	
     return uki.build(val);
 };
 
@@ -39,9 +42,9 @@ uki.F = function() { return false; };
 uki._ids = {};
 
 uki.registerId = function(comp) {
-    uki._ids[uki.attr(comp, 'id')] = comp;
+    uki._ids[ uki.attr(comp, 'id') ] = comp;
 }; 
 uki.unregisterId = function(comp) {
-    uki._ids[uki.attr(comp, 'id')] = undefined;
+    uki._ids[ uki.attr(comp, 'id') ] = undefined;
 };
 
