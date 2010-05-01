ed3e825c35e4029ada01d2ec192956b56d10cb28
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-01 19:58:46 +0700

    View: loops optimizations

--- src/uki-core/view.js
+++ src/uki-core/view.js
@@ -7,15 +7,16 @@ uki.view = {
             klass = uki.newClass.apply(uki, args),
             parts = name.split('.'),
             obj   = root,
-            i, part;
+            i, part, $partslen = parts.length;
         
         klass.prototype.typeName = function() { return name; };
-        for ( i=0; i < parts.length - 1; i++ ) {
+        for ( i= $partslen - 2; i >= 0; i-- ) {
             part = parts[i];
             if (!obj[part]) obj[part] = {};
             obj = obj[part];
         };
-        obj[parts[parts.length - 1]] = klass;
+		
+        obj[ parts[$partslen - 1] ] = klass;
         return klass;
     }
 };
