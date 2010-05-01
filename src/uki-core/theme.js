a2b486f93b5e338ee11373af6783b9f08eac4f00
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-01 20:06:29 +0700

    theme.js: Wrapped all with scope that holds uki variable for fast access, plus optimized loop

--- src/uki-core/theme.js
+++ src/uki-core/theme.js
@@ -3,43 +3,43 @@ include('uki.js');
 /**
  * @namespace
  */
-uki.theme = {
+uki$theme = (function (uki$theme) { return uki$theme = {
     themes: [],
     
-    register: function(theme) {
-        uki.theme.themes.push(theme);
+    register: function(theme, /* internal */ themes) {
+        (themes = uki$theme.themes)[ themes.length] = theme;
     },
     
     background: function(name, params) {
-        return uki.theme._namedResource(name, 'background', params) || new uki.background.Null();
+        return uki$theme._namedResource(name, 'background', params) || new uki.background.Null();
     },
     
     image: function(name, params) {
-        return uki.theme._namedResource(name, 'image', params) || new Image();
+        return uki$theme._namedResource(name, 'image', params) || new Image();
     },
     
     imageSrc: function(name, params) {
-        return uki.theme._namedResource(name, 'imageSrc', params) || '';
+        return uki$theme._namedResource(name, 'imageSrc', params) || '';
     },
     
     style: function(name, params) {
-        return uki.theme._namedResource(name, 'style', params) || '';
+        return uki$theme._namedResource(name, 'style', params) || '';
     },
     
     dom: function(name, params) {
-        return uki.theme._namedResource(name, 'dom', params) || uki.createElement('div');
+        return uki$theme._namedResource(name, 'dom', params) || uki.createElement('div');
     },
     
     template: function(name, params) {
-        return uki.theme._namedResource(name, 'template', params) || '';
+        return uki$theme._namedResource(name, 'template', params) || '';
     },
     
-    _namedResource: function(name, type, params) {
-        for (var i = uki.theme.themes.length - 1; i >= 0; i--){
-            var result = uki.theme.themes[i][type](name, params);
-            if (result) return result;
+    _namedResource: function(name, type, params, i, result) {
+        for ( i = uki$theme.themes.length - 1, ; i >= 0; i--) {
+            if (result = uki$theme.themes[i] [type](name, params))
+				return result;
         };
         return null;
         
     }
-};
+}; })();
