ff0e9a01241bccdf964787c6f1dbce1f28a527b7
Володя Колесников <voloko@gmail.com>
2010-05-11 13:17:08 +0400

    form view by grgh@

--- src/uki-more.js
+++ src/uki-more.js
@@ -3,5 +3,6 @@ include('uki-more/more/view/treeList.js');
 include('uki-more/more/view/toggleButton.js');
 include('uki-more/more/view/radioButton.js');
 include('uki-more/more/view/splitTable.js');
+include('uki-more/more/view/form.js');
 include('uki-more/more/view/select.js');
 include('uki-more/more/view/select-theme.js');
---
+++ src/uki-more/more/view/form.js
@@ -0,0 +1,29 @@
+include('../view.js');
+
+uki.view.declare('uki.more.view.Form', uki.view.Container, function(Base) {
+    
+    this._setup = function() {
+        Base._setup.call(this);
+        uki.extend(this, {
+            _method: 'GET',
+            _action: ''
+        });
+    };
+    
+    this.action = uki.newProp('_action', function(action) {
+      this._dom.action = this._action = action;
+    });
+    this.method = uki.newProp('_method', function(method) {
+      this._dom.method = this._method = method;
+    });
+    
+    this.submit = function() { this._dom.submit(); }
+    this.reset = function() { this._dom.reset(); }
+    
+    this._createDom = function() {
+        this._dom = uki.createElement('form', Base.defaultCss);
+        this._dom.action = this._action;
+        this._dom.method = this._method;
+    };
+   
+});
