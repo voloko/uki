3e4929d251c593e888584ac8b8c3eab2c3c87664
Володя Колесников <voloko@gmail.com>
2010-06-24 13:39:54 +0400

    more classes, use div insead of button. to much hasle

--- src/uki-view/view/image.js
+++ src/uki-view/view/image.js
@@ -16,6 +16,7 @@ uki.view.declare('uki.view.Image', uki.view.Base, function() {
     uki.delegateProp(this, 'src', '_dom');
     
     this._createDom = function() {
-        this._dom = uki.createElement('img', this.defaultCss)
+        this._dom = uki.createElement('img', this.defaultCss);
+        this._initClassName();
     };
 });
