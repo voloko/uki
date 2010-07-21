1b63144f7fac05d4865dd43b6e1d12f4affeb6f8
Roberto Saccon <rsaccon@gmail.com>
2010-07-21 10:05:42 -0300

    more upstreamed changes to persistencejs, now persitence.sync can also be used with uki (and uses uki akax implementation)

--- src/uki-data/uki-persistence.js
+++ src/uki-data/uki-persistence.js
@@ -1,38 +1,36 @@
 /**
  * persistencejs integration (http://www.persistencejs.org)
  * 
- * (persistence.js from http://github.com/rsaccon/persistencejs)
- *  
- * Example:
- *
- * // persistence engine
- * include('/path/to/persistence.js'); 
- * include('/path/to/persistence.sync.js'); // optional
- * include('/path/to/persistence.search.js'); // optional
- * include('/path/to/persistence.migrations.js'); // optional
- * include('/path/to/uki-data/uki-persistence.js');
- * uki.persistence = persistence;
- *                  
- * uki.persistence.connect('myDbName', 'database', 5 * 1024 * 1024, '1.0'); 
- *                          
- * var User = uki.persistence.define('User', {
- *   firstname: "TEXT",
- *   lastname: "TEXT"
- * });
- *
- * var aUser = new User();
- *
- * aUser.firstname("Mike") ;
- *
- * console.log(aUser.firstname()); // => Mike 
- *
- * uki.persistence.add(aUser);
- * 
- * uki.persistence.flush();
- * 
- */
+ **/ 
  
- persistence.defineProp = function(scope, field, setterCallback, getterCallback) {
+// Example
+// =======
+// // persistence engine
+// include('path/to/persistence.js'); 
+// include('path/to/persistence.sync.js'); // optional
+// include('path/to/persistence.search.js'); // optional
+// include('path/to/persistence.migrations.js'); // optional
+// include('path/to/uki-data/uki-persistence.js');
+//                  
+// persistence.connect('myDbName', 'database', 5 * 1024 * 1024, '1.0'); 
+//                          
+// var User = uki.persistence.define('User', {
+//   firstname: "TEXT",
+//   lastname: "TEXT"
+// });
+// 
+// var aUser = new User();
+// 
+// aUser.firstname("Mike") ;
+// 
+// console.log(aUser.firstname()); // => Mike 
+// 
+// persistence.add(aUser);
+// 
+// persistence.flush();
+
+
+persistence.defineProp = function(scope, field, setterCallback, getterCallback) {
     scope[field] = function(val) {
         if (val === undefined) { 
             return getterCallback();
@@ -46,3 +44,32 @@
 persistence.entityPropToEntityVal = function(val) { 
     return val(); 
 };
+ 
+if (persistence.sync) { 
+    persistence.sync.get = function(uri, onSuccess, onError) {
+        uki.ajax({
+            url: uri,
+            type: 'GET',
+            success: function(response) {
+                if (onSuccess) onSuccess(response);
+            },
+            error: function(response) {
+                if (onError) onError(response); 
+            }
+        });
+    }
+    persistence.sync.post = function(uri, data, successCallback, errorCallback) {
+        uki.ajax({
+            url: uri,
+            type: 'POST',
+            data: data, 
+            dataType: 'json', 
+            success: function(response) {
+                if (onSuccess) onSuccess(response);
+            },
+            error: function(response) {
+                if (onError) onError(response); 
+            }
+        });
+    } 
+}
