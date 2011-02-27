4cb9c982bf550c87bb2cd89ab44751fc3b253d1c
Vladimir Kolesnikov <voloko@gmail.com>
2011-02-26 19:39:59 -0800

    text -> label for button

--- examples/core-examples/splitPane/splitPane.js
+++ examples/core-examples/splitPane/splitPane.js
@@ -8,12 +8,12 @@
 var uki = require('uki');
 
 uki({ view: 'SplitPane', pos: 'l:0 t:0 r:0 b:0', init: { vertical: true },
-    leftChildViews: [{ view: 'Button', text: 'left' }],
+    leftChildViews: [{ view: 'Button', label: 'left' }],
     rightChildViews: [
         { view: 'SplitPane', pos: 'l:0 t:0 r:0 b:0', handlePosition: 300,
             init: { handleWidth: 5 },
-            leftChildViews: [ { view: 'Button', text: 'top' } ], 
-            rightChildViews: [ { view: 'Button', text: 'bottom' } ] 
+            leftChildViews: [ { view: 'Button', label: 'top' } ], 
+            rightChildViews: [ { view: 'Button', label: 'bottom' } ] 
         }
     ]
 }).attach();
