a6a1dcf951feb9937ba953626b24187d063da319
Vladimir Kolesnikov <voloko@gmail.com>
2011-03-22 11:15:44 -0700

    button styling

--- examples/core-examples/attaching/attaching.js
+++ examples/core-examples/attaching/attaching.js
@@ -2,7 +2,7 @@
 @example_title Hello world
 @example_order 1
 @example_html
-    <div id='test' style='width: 50%; height: 100px; background: #EEE; position:relative'>#test</div>
+    <div id='test' style='width: 50%; height: 100px; background: #EEE; position:relative; background: -webkit-gradient(linear, 0 0,0 100%, from(#E6E6E6), color-stop(80%,#ccc), to(#cfcfcf));'></div>
     <script src="/src/pkg/uki.js"></script>
     <script src="attaching.js"></script>
 */
